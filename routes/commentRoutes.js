880b8d72b0bd7003540a21c6c5e47592ddb8f2e3
aaghaz afzal <aghazafzal8294@gmail.com>
2025-11-08 04:00:18 +0530

    Create commentRoutes.js

---
+++ routes/commentRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  getCommentsByBook,
+  addComment,
+  replyToComment,
+} from "../controllers/commentController.js";
+
+const router = express.Router();
+
+// GET comments for a book
+router.get("/:bookId", getCommentsByBook);
+
+// POST a new comment
+router.post("/", addComment);
+
+// POST a reply to an existing comment
+router.post("/reply/:commentId", replyToComment);
+
+export default router;
