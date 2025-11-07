603deb0bd80eb3de5e7e1415bf6d0bcd04840a50
aaghaz afzal <aghazafzal8294@gmail.com>
2025-11-08 03:55:48 +0530

    Create comment.js

---
+++ models/comment.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const replySchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  text: { type: String, required: true, trim: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const commentSchema = new mongoose.Schema(
+  {
+    bookId: { type: mongoose.Schema.Types.ObjectId, ref: "Book", required: true },
+    name: { type: String, required: true, trim: true },
+    text: { type: String, required: true, trim: true },
+    replies: [replySchema],
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+export default Comment;
