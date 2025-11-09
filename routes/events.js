cfd5cfe2b001b207ed126021d8982d5dd36e2ac9
aaghaz afzal <aghazafzal8294@gmail.com>
2025-11-09 09:46:07 +0530

    Update events.js

--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { recordEvent } from "../controllers/eventsController.js";
 import { getAchievements } from "../controllers/achievementsController.js";
-+import { protect } from "../middleware/authMiddlewareWebsite.js";
+import { protect } from "../middleware/authMiddlewareWebsite.js";
 
 const router = express.Router();
 
