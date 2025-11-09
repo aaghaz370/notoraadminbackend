cfc43e2e76a62cd7e8d906c1c9e02c72884e8827
aaghaz afzal <aghazafzal8294@gmail.com>
2025-11-09 10:10:25 +0530

    Update authMiddlewareWebsite.js

--- middleware/authMiddlewareWebsite.js
+++ middleware/authMiddlewareWebsite.js
@@ -18,8 +18,10 @@ export const protect = async (req, res, next) => {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = await User.findById(decoded.id).select("-password");
-      if (!req.user)
-        return res.status(404).json({ message: "User not found" });
+      if (!req.user) {
+  return res.status(401).json({ message: "Unauthorized: user not found" });
+}
+
 
       return next();
     }
