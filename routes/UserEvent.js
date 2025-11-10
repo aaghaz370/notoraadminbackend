ca42c71242b91c63e0849fc67027bacfa5e51192
aaghaz afzal <aghazafzal8294@gmail.com>
2025-11-10 22:11:43 +0530

    Create UserEvent.js

---
+++ routes/UserEvent.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userEventSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["read", "share", "donate", "admin_bonus"], // include admin bonus
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    points: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const UserEvent = mongoose.model("UserEvent", userEventSchema);
+export default UserEvent;
