87950f6a7a72b4fdcafd984481dc19e2a63c3678
aaghaz afzal <aghazafzal8294@gmail.com>
2025-11-15 02:12:12 +0530

    Update notifications.js

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { sendNotification, getUserNotifications } from "../controllers/notificationsController.js";
-import { protect, adminOnly } from "../middleware/authMiddlewareWebsite.js";
+// import { protect, adminOnly } from "../middleware/authMiddlewareWebsite.js";
 
 const router = express.Router();
 
 // 🟥 ADMIN — send notification
-router.post("/send", protect, adminOnly, sendNotification);
+router.post("/send", sendNotification);
+
 
 // 🟩 USER — get their notifications
 router.get("/my", protect, getUserNotifications);
